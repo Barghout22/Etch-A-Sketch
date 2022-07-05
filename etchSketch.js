3297e0b767c085eaae62765ae2361a967e7fde13
Mahmoud <barghoutmahmoud2@gmail.com>
2022-07-05 16:41:04 +0200

    created a working model for changing colors when hovering. created a function to add the divs depending on the users choice through a button and a prompt. created a button to clear the color off all items. the function to change the number of divs on screen doesn't allow us to recolor them afterwards for some reason.

--- etchSketch.js
+++ etchSketch.js
@@ -1,20 +1,34 @@
 
-let gridNumber=prompt('enter the grid measurements');
-
-let expectedMeasurment=960/gridNumber;
+let gridNumber=64;
 let numberOfItems=gridNumber**2;
 const gridContainer=document.querySelector('.container');
 
-for(let i=1;i<=numberOfItems;i++)
+function addElements(numberOfItems)
 {
+    let expectedMeasurment=960/gridNumber;
+
+    for(let i=1;i<=numberOfItems;i++)
+{
+
 const div=document.createElement('div');
 div.classList.add('grid-div');
 div.setAttribute('id',`${i}`);
-div.textContent=`${i}`;
 div.style.width=`${expectedMeasurment}px`;
 div.style.height=`${expectedMeasurment}px`;
 gridContainer.appendChild(div);
 }
+
+}
+
+function clearElements()
+{
+  while(gridContainer.firstChild)
+  {
+    gridContainer.removeChild(gridContainer.lastChild);
+  }
+}
+
+addElements(numberOfItems);
 
 /*function changeColor(e)
 {
@@ -24,7 +38,34 @@ this.style.backgroundColor='orange';
 }
 */
 
-let id =document.getElementsByTagName('div')[1].id;
-console.log(id);
+function activateButton(Class)
+{
+if(Class==='set') 
+{
+   /* numberOfItems=0;
+    gridNumber=prompt('enter the grid measurements\n this button needs to adjust to delete old divs and create new divs');
+    numberOfItems=gridNumber**2;
+    console.log(gridNumber);
+    grids.forEach(grid=>grid.classList.remove('hover'));
+    clearElements();
+    addElements(numberOfItems);
+    console.log(numberOfItems);
+*/
+}
 
-div.addEventListener('onclick',console.log('hello world'));
+else if(Class==='clear')
+{
+    grids.forEach(grid=>grid.classList.remove('hover'));
+}
+}
+
+const grids=document.querySelectorAll('.grid-div');
+//grids.forEach(div=>console.log(div.getAttribute(id)));
+grids.forEach(grid=>grid.addEventListener('mouseenter',()=>{
+    grid.classList.add('hover');
+}));
+
+const buttons=document.querySelectorAll('button');
+buttons.forEach(button=>button.addEventListener('click',()=>{
+    activateButton(button.classList.value);
+}));
